cc75eb3dff509e6c38c375ad6f45dd59fff257bc
prranavv <pranavvkumar03@gmail.com>
2025-03-29 15:01:06 +0530

    added multiple inputs for basename_rust

--- basename_rust/src/main.rs
+++ basename_rust/src/main.rs
@@ -5,17 +5,39 @@ use clap::Parser;
 #[command(about="Print NAME with any leading directory components removed",long_about=None)]
 #[command(version="1.0")]
 struct Cli{
-    name: String
+    name: Vec<String>,
+    /// support multiple arguments and treat each as a NAME
+    #[arg(short='a')]
+    #[arg(long="multiple")]
+    multiple_inputs: bool
 }
 
 fn main(){
     let cli=Cli::parse();
+    
+    match cli.multiple_inputs{
+        false=>{
+            let path = &cli.name[0];
+            let iter =path.split("/");
+            let i =iter.last();
+            match i{
+                Some(val)=>println!("{}",val),
+                _=>(),
+            }
+        },
+        true=>{
+            let path = cli.name;
+            let i =path.iter();
+            for val in i{
+                let iter = val.split("/");
+                let x = iter.last();
+                match x{
+                    Some(val)=>println!("{}",val),
+                    _=>(),
+                }
+            }
+        }
 
-    let path = cli.name;
-    let iter =path.split("/");
-    let i =iter.last();
-    match i{
-        Some(val)=>println!("{}",val),
-        _=>(),
     }
+    
 }
