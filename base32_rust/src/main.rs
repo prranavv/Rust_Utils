6689a2921abc5ebefa2f4faacffb1c996f0c46d4
prranavv <pranavvkumar03@gmail.com>
2025-03-30 18:37:01 +0530

    fixed the encode implementation

--- base32_rust/src/main.rs
+++ base32_rust/src/main.rs
@@ -1,11 +1,16 @@
 use clap::Parser;
-use base32::{encode,Alphabet};
-
+use base32::{decode, encode, Alphabet};
+use std::path::Path;
+use std::fs;
 #[derive(Parser)]
 #[command(name="base32_rust",version="1.0",
 about="encode/decode data and print to standard output With no FILE, or when FILE is -, read standard input.")]
 struct Cli{
-    file_path: Option<String>
+    file_path: Option<String>,
+
+    /// decode data
+    #[arg(short='d',long="decode")]
+    decode: bool
 }
 
 fn main(){
@@ -18,8 +23,28 @@ fn main(){
             std::process::exit(-1);
         }
     };
-    let bytes_file_path = file_path.as_bytes();
-    let a =Alphabet::Rfc4648 { padding: false };
-    let encoded_bytes = encode(a, bytes_file_path);
-    println!("{}",encoded_bytes);
+    let path=Path::new(&file_path);
+    let f = fs::read_to_string(path).unwrap();
+    match cli.decode{
+        false=>{
+            let a =Alphabet::Rfc4648 { padding: false };
+            let bytes_file_path = f.as_bytes();
+            let encoded_bytes = encode(a, bytes_file_path);
+            println!("{}",encoded_bytes);
+            
+        },
+        true=>{
+            let a = Alphabet::Rfc4648 { padding: false };
+            let decoded_bytes_op = decode(a, &file_path);
+            let decoded_bytes=match decoded_bytes_op{
+                Some(val)=>val,
+                None=>{
+                    std::process::exit(-1);
+                }
+            };
+            let result=String::from_utf8(decoded_bytes).unwrap();
+            println!("{}",result);
+        }
+    }
+    
 }
