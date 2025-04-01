3330427fa3c603633753532d6efb40ea7cde7de5
prranavv <pranavvkumar03@gmail.com>
2025-04-01 23:58:49 +0530

    added the flag to make parent directories in mkdir_rust

--- mkdir_rust/src/main.rs
+++ mkdir_rust/src/main.rs
@@ -7,6 +7,10 @@ use clap::Parser;
 struct Cli{
     /// Enter Dir name to be created
     dir_name: Option<String>,
+
+    /// make parent directories as needed
+    #[arg(short='p',long="parents")]
+    parent: bool
 }
 
 fn main(){
@@ -21,11 +25,18 @@ fn main(){
             std::process::exit(-1);
         }
     };
-    let r = fs::create_dir(dir_name);
-    match r{
-        Err(e)=>{
-            println!("{}",e)
+    let r =match cli.parent{
+        false=>{
+            fs::create_dir(dir_name)
+        },
+        true=>{
+            fs::create_dir_all(dir_name)
         }
+    };
+    match r {
+        Err(e)=>println!("{}",e),
         _=>()
     }
+    
+
 }
