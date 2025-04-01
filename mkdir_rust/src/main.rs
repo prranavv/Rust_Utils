a458c6f5cb4d80bcb123f7a1a7b3d696e3a1e652
prranavv <pranavvkumar03@gmail.com>
2025-04-02 00:17:14 +0530

    added the flag to print verbose messages for each printed directory in mkdir_rust

--- mkdir_rust/src/main.rs
+++ mkdir_rust/src/main.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{fs, path::Path};
 
 use clap::Parser;
 
@@ -10,7 +10,11 @@ struct Cli{
 
     /// make parent directories as needed
     #[arg(short='p',long="parents")]
-    parent: bool
+    parent: bool,
+
+    /// print a verbose messaage for each printed directory
+    #[arg(short='v',long="verbose")]
+    verbose: bool
 }
 
 fn main(){
@@ -27,16 +31,37 @@ fn main(){
     };
     let r =match cli.parent{
         false=>{
+            match cli.verbose{
+                false=>(),
+                true=>{
+                    println!("mkdir: created {}",dir_name);
+                }
+            }
             fs::create_dir(dir_name)
         },
         true=>{
+            match cli.verbose{
+                false=>(),
+                true=>{
+                    let p=Path::new(&dir_name);
+                    let ancestors=p.ancestors();
+                    for a in ancestors{
+                        if a.is_dir(){
+                            break;
+                        }
+                        println!("mkdir: created {}",a.display());
+                    }
+                }
+            }
+            
             fs::create_dir_all(dir_name)
         }
     };
     match r {
-        Err(e)=>println!("{}",e),
-        _=>()
-    }
-    
-
+        Err(e)=>{
+            println!("{}",e);
+            std::process::exit(-1);
+        },
+        Ok(_)=>{}
+    };
 }
