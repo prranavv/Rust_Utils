1731093ef936f4dc6429acb930260821d0d7c154
prranavv <pranavvkumar03@gmail.com>
2025-04-05 23:24:49 +0530

    added the rust implementation of cp

--- cp_rust/src/main.rs
+++ cp_rust/src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+use std::{fs, path::Path};
+
+use clap::Parser;
+use anyhow::Result;
+
+#[derive(Parser)]
+#[command(about="Copy SOURCE to DEST, or multiple SOURCE(s) to DIRECTORY.")]
+#[command(name="cp")]
+struct Cli{
+    source: Option<String>,
+    destination: Option<String>
 }
+
+fn main()-> Result<()>{
+    let cli = Cli::parse();
+    let source =match cli.source{
+        Some(val)=>val,
+        None=>{
+            std::process::exit(-1);
+        },
+    };
+    
+    let destination =match cli.destination{
+        Some(val)=>val,
+        None=>{
+            std::process::exit(-1);
+        },
+    };
+
+    let source_path = Path::new(&source);
+    let destination_path = Path::new(&destination);
+
+    fs::rename(source_path, destination_path)?;
+
+    Ok(())
+}
+
+
