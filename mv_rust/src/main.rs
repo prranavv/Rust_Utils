59cbd1b035c4cfa8bde05aaf47089a73013b353c
prranavv <pranavvkumar03@gmail.com>
2025-04-06 00:00:59 +0530

    added an option to the cli args

--- mv_rust/src/main.rs
+++ mv_rust/src/main.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{fs, io::{stdin, stdout, Write}};
 
 use clap::Parser;
 use anyhow::Result;
@@ -7,7 +7,11 @@ use anyhow::Result;
 #[command(about="Move SOURCE to DEST, or multiple SOURCE(s) to DIRECTORY.")]
 struct Cli{
     source: Option<String>,
-    destination: Option<String>
+    destination: Option<String>, 
+
+    /// prompt before override
+    #[arg(short='i',long="interactive")]
+    interactive: bool
 }
 
 fn main()->Result<()>{
@@ -15,6 +19,23 @@ fn main()->Result<()>{
     let source = cli.source.expect("expected a valid source");
     let destination = cli.destination.expect("expected a valid destinatoin");
 
+    match cli.interactive{
+        true=>{
+            let mut s =String::new();
+            print!("mv-rust: overwrite '{}'",destination);
+            let _ = stdout().flush();
+            stdin().read_line(&mut s)?;
+            let s =s.trim();
+            match s{
+                "y" |"yes" =>{
+
+                }
+
+                _=> std::process::exit(-1)
+            }
+        }
+        false=>std::process::exit(-1)
+    }
     fs::rename(source, destination)?;
     Ok(())
 }
